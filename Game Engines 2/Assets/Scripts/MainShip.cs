ad2d28d0ec7d3931d3c7f040fe48c3399333c3ea
Zeppelin03 <56638997+Zeppelin03@users.noreply.github.com>
2020-05-19 19:17:08 +0100

    New ship scripts + particle effects

---
+++ Game Engines 2/Assets/Scripts/MainShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        var pos = transform.position;
+        pos.z += 0.1f;
+        transform.position = pos;
+    }
+}
---
+++ Game Engines 2/Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+
+    public GameObject Ship;
+    public GameObject Beam1;
+    public GameObject Beam2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ship.GetComponent<MainShip>().enabled = false;
+        Beam1.SetActive(true);
+        Beam2.SetActive(true);
+    }
+}
