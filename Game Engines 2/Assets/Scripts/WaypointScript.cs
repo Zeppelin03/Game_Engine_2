88f326831afdbc84a3b9d79268396b4cb6feade0
Zeppelin03 <56638997+Zeppelin03@users.noreply.github.com>
2020-05-19 23:23:11 +0100

    Added particles + drone ships

--- Game Engines 2/Assets/Scripts/WaypointScript.cs
+++ Game Engines 2/Assets/Scripts/WaypointScript.cs
@@ -8,11 +8,14 @@ public class WaypointScript : MonoBehaviour
     public GameObject Ship;
     public GameObject Beam1;
     public GameObject Beam2;
+    public GameObject Smoke;
 
     private void OnTriggerEnter(Collider other)
     {
         Ship.GetComponent<MainShip>().enabled = false;
         Beam1.SetActive(true);
         Beam2.SetActive(true);
+        Smoke.SetActive(true);
+
     }
 }
